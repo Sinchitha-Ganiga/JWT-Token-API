9f4e5c8cf6c8e47d7fa8881c69f0d2ad86696059
Sinchitha Ganiga <137211740+Sinchitha-Ganiga@users.noreply.github.com>
2023-07-23 13:39:29 +0530

    Initial Commit

---
+++ JWTTokenAPI/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JWTTokenAPI.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage ValidateLogin(string userName, string userPassword)
+        {
+            if (userName == "admin" || userPassword == "12345")
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, TokenManager.GenerateToken(userName));
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "UserName and Password is invalid");
+            }
+        }
+    }
+}
---
+++ JWTTokenAPI/TokenManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace JWTTokenAPI
+{
+    public class TokenManager
+    {
+        private static string Secret = "MjUxQjRFN0UtNUM4Mi00MDI4LUE0MUEtMkQwMDUzMjhGMDQ0"; //Guid.NewGuid().ToString();   //GUID: Creates a unique identifier
+
+        public static string GenerateToken(string userName)
+        {
+            byte[] key = Convert.FromBase64String(Secret);
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims: new[] { new Claim(type: ClaimTypes.Name, userName) }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(securityKey, algorithm: SecurityAlgorithms.HmacSha256Signature)
+                               
+            };
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = handler.CreateJwtSecurityToken(tokenDescriptor);
+
+            return handler.WriteToken(token);
+        }
+    }
+}
